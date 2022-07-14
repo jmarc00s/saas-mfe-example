83667e08c8440c77393aa7077e9e7d5927c49cc6
João Marcos <jmarcossferreira@hotmail.com>
2022-07-14 13:06:36 -0300

    Implement marketing routes

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { StylesProvider } from "@material-ui/core/styles";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Pricing from "./components/Pricing";
+import Landing from "./components/Landing";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <StylesProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route exact path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
+    </>
+  );
 };
 
 export default App;
