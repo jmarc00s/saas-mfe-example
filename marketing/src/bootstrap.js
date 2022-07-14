6e624489cf9d7fa6263d05e6208408f86c8d1656
João Marcos <jmarcossferreira@hotmail.com>
2022-07-14 12:59:53 -0300

    Configure react in marketing app

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+if (process.env.NODE_ENV === "development") {
+  const element = document.querySelector("#_marketing_dev_root");
+  mount(element);
+}
+
+export { mount };
