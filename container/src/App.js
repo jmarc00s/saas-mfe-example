6e02acd608d0f63302b756afd0f1fe5385467927
João Marcos <jmarcossferreira@hotmail.com>
2022-07-15 12:51:10 -0300

    Create integration between container and marketing

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
-
-console.log(mount);
+import MarketingApp from "./components/MarketingApp";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
 
 export default App;
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -6,7 +6,10 @@ const mount = (el) => {
   ReactDOM.render(<App />, el);
 };
 
-if (process.env.NODE_ENV === "development") {
+if (
+  process.env.NODE_ENV === "development" &&
+  document.querySelector("#_marketing_dev_root")
+) {
   const element = document.querySelector("#_marketing_dev_root");
   mount(element);
 }
