3ac0551144842a23194d55b5119d1519b578802d
João Marcos <jmarcossferreira@hotmail.com>
2022-07-18 21:00:37 -0300

    Configure container webpack for production

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
@@ -13,9 +12,6 @@ const devConfig = {
     },
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
