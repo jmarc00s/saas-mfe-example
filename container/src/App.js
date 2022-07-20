745fb0883470333f962ebc751a4cc0946aa70e04
João Marcos <jmarcossferreira@hotmail.com>
2022-07-19 21:27:31 -0300

    Change page title

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
     <div>
-      <h1>Hello from container App!</h1>
+      <h1>Hello from container App! using github action!</h1>
       <hr />
       <MarketingApp />
     </div>
